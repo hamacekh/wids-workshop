7e359a5bdf685c78b9a09b792dd712e45296f2af
Jan Hamacek <jenda@hamacek.net>
2023-02-09 21:05:35 +0100

    Vectorize images

--- src/helpers.py
+++ src/helpers.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
+from torch import nn
 from torchvision import transforms
 from PIL import Image
 
 
-def image_to_vector(img_path: str, model) -> np.array:
+def image_to_vector(img_path: str, model: nn.Module) -> np.array:
     preprocess = transforms.Compose(
         [
             transforms.Resize(256),
@@ -13,22 +14,22 @@ def image_to_vector(img_path: str, model) -> np.array:
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     )
-    input_image = # load the image from img_path
+    input_image = Image.open(img_path)
     input_tensor = preprocess(input_image)
     input_batch = input_tensor.unsqueeze(0)
-    vector = # use the model to create a vector representation of the input_batch
+    vector = model(input_batch)[0].detach().numpy()  # use the model to create a vector representation of the input_batch
 
     return vector
 
 
-def most_similar_vectors(image_vector: np.array, df_vectors: pd.DataFrame) -> str:
-    min_dist = float("inf")
-    closest_breed = None
+#def most_similar_vectors(image_vector: np.array, df_vectors: pd.DataFrame) -> str:
+#    min_dist = float("inf")
+#    closest_breed = None
+#
+#    for i, row in df_vectors.iterrows():
+#        dist = # compare the distance of the two vectors
+#        if dist < min_dist:
+#            min_dist = dist
+#            closest_breed = row["img_path"]
 
-    for i, row in df_vectors.iterrows():
-        dist = # compare the distance of the two vectors
-        if dist < min_dist:
-            min_dist = dist
-            closest_breed = row["img_path"]
-
-    return closest_breed
+#    return closest_breed
